10c641435d63f04c6018c1dd7c76ef5e534c05d5
nguyenhuuvinh <nguyenhuuvinh@gmail.com>
2026-01-13 14:44:56 +0700

    fix: add log info in health

--- hex/src/main/java/com/arch/hex/system/inbound/rest/controller/actuator/ActuatorController.java
+++ hex/src/main/java/com/arch/hex/system/inbound/rest/controller/actuator/ActuatorController.java
@@ -1,6 +1,7 @@
 package com.arch.hex.system.inbound.rest.controller.actuator;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.health.actuate.endpoint.HealthDescriptor;
 import org.springframework.boot.health.actuate.endpoint.HealthEndpoint;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ActuatorController {
     private final HealthEndpoint healthEndpoint;
 
     @GetMapping("/health")
     public HealthDescriptor health(){
+        log.info("APP VERSION = 1.0.4");
         return healthEndpoint.health();
     }
 }
